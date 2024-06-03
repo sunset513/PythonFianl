56515dde9870ef4027356e2ae5026ec1b673c350
sunset513 <amy39899009@gmail.com>
2024-06-04 02:19:05 +0800

    add: still in progress

---
+++ README.txt
@@ -0,0 +1,35 @@
+# Python 期末專題 Gemini 網站 clone
+
+## 一 安裝
+[確保你有下載 Python]
+
+[確保你有下載 pip]
+
+如果你已經安裝了 Python 和 pip，請在終端或命令行工具中檢查它們的版本
+
+```
+python3 --version
+```
+
+```
+pip --version
+```
+
+## 二 安裝必要套件
+打開終端機
+透過 requirements.txt 安裝必要套件
+
+```
+pip install -r requirements.txt
+```
+
+
+## 三 開始使用
+
+```
+cd <檔案位置>
+```
+
+```
+python main.py
+```
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from flask import Flask, request, render_template, url_for, redirect, g, session, send_file
+from flask import Flask, request, render_template, url_for, redirect, g, session, send_file,jsonify
 from flask_wtf import FlaskForm
+
+# 資料庫及表單
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError, Regexp, Length
 import sqlite3
 
+# 大型語言模型
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
+# 資料庫處理
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -20,6 +31,7 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+# 判斷使用者相關資訊
 def validate_username(form, field):
     db = get_db()
     user = db.execute('SELECT * FROM users WHERE username = ?', (field.data,)).fetchone()
@@ -82,6 +94,15 @@ def login():
     form.password.data = ''
     return render_template('login.html', form=form)
 
+
+# 登出
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+
+# 聊天室
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
     if 'username' not in session:
@@ -90,11 +111,28 @@ def chat():
     
     return render_template('chat.html')
 
+# 聊天室訊息
+@app.route('/get', methods=['GET', 'POST'])
+def chat():
+    message =request.form['message']
+    input = message
+    return get_response(input)
 
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('login'))
+def get_response(inputText):
+    for step in range(5):
+
+        # encode the new user input, add the eos_token and return a tensor in Pytorch
+        new_user_input_ids = tokenizer.encode(str(inputText) + tokenizer.eos_token, return_tensors='pt')
+
+        # append the new user input tokens to the chat history
+        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+
+        # generated a response
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+        # pretty print last ouput tokens from bot
+        return format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
